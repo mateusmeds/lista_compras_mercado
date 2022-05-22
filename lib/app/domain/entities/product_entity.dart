4660db2259dae020fc291fef3174889c540773c8
Antonio Mateus de Souza Medeiros <mateus.medeiros@altislab.com.br>
2022-05-21 22:48:46 -0300

    Adicionado modelo de entidade de Produto

---
+++ lib/app/domain/entities/product_entity.dart
@@ -0,0 +1,31 @@
+class ProductEntity {
+  ProductEntity({
+    required String name,
+    required double price,
+    int quantity = 1,
+  })  : _name = name,
+        _price = price,
+        _quantity = quantity;
+
+  String _name;
+  double _price;
+  int _quantity;
+
+  String get name => _name;
+  double get price => _price;
+  int get quantity => _quantity;
+
+  double get totalPrice => _getTotalPrice();
+
+  double _getTotalPrice() {
+    return _price * _quantity;
+  }
+
+  remove() {
+    _quantity--;
+  }
+
+  add() {
+    _quantity++;
+  }
+}
