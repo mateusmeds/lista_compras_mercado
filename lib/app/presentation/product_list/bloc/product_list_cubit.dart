69747d50b9528047090fe4389834ece09d7efda5
Antonio Mateus de Souza Medeiros <mateus.medeiros@altislab.com.br>
2022-05-22 03:15:01 -0300

    Adicionado dados dinâmicos

---
+++ lib/app/presentation/product_list/bloc/product_list_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lista_compras_mercado/app/domain/entities/product_entity.dart';
+
+class ProductListCubit extends Cubit<List<ProductEntity>> {
+  ProductListCubit(List<ProductEntity> productEntityList)
+      : super(productEntityList);
+
+  double get totalValue {
+    return state.fold<double>(
+        0, (previousValue, element) => previousValue + element.totalPrice);
+  }
+
+  void add(ProductEntity productEntity) {
+    state.add(productEntity);
+    emit(List.of(state));
+  }
+
+  void incrementProduct(int index) {
+    state[index].add();
+    emit(List.of(state));
+  }
+
+  void decrementProduct(int index) {
+    state[index].remove();
+    emit(List.of(state));
+  }
+}
--- lib/app/presentation/product_list/pages/product_list_page.dart
+++ lib/app/presentation/product_list/pages/product_list_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:lista_compras_mercado/app/domain/entities/product_entity.dart';
+import 'package:lista_compras_mercado/app/presentation/product_list/bloc/product_list_cubit.dart';
+import 'package:lista_compras_mercado/app/presentation/product_list/pages/form_product_page.dart';
 
 class ProductListPage extends StatefulWidget {
   const ProductListPage({Key? key}) : super(key: key);
@@ -8,6 +12,15 @@ class ProductListPage extends StatefulWidget {
 }
 
 class _ProductListPageState extends State<ProductListPage> {
+  late List<ProductEntity> _productEntityList;
+  late ProductListCubit _productListCubit;
+  @override
+  void initState() {
+    _productEntityList = [];
+    _productListCubit = ProductListCubit(_productEntityList);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,91 +29,135 @@ class _ProductListPageState extends State<ProductListPage> {
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () {},
-            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return FormProductPage(_productListCubit);
+              }));
+            },
+            icon: const Icon(Icons.add),
             splashRadius: 20,
           )
         ],
       ),
       body: LayoutBuilder(
         builder: (context, constraints) => Center(
-          child: Container(
+          child: SizedBox(
             width: constraints.maxWidth * 0.8,
             child: Column(
               children: [
+                SizedBox(
+                  height: 20,
+                ),
                 Expanded(
-                  child: ListView.builder(
-                    itemCount: 2,
-                    itemBuilder: (context, index) => Container(
-                      margin: EdgeInsets.only(bottom: 15),
-                      child: ListTile(
-                        contentPadding:
-                            EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-                        tileColor: Colors.grey[200],
-                        leading: CircleAvatar(),
-                        title: Text('Item $index'),
-                        subtitle: Container(
-                          margin: EdgeInsets.only(top: 10),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text('Preço: R\$ 10,00'),
-                              SizedBox(
-                                height: 5,
-                              ),
-                              Text('Total:  R\$ 20,00'),
-                            ],
-                          ),
-                        ),
-                        trailing: FittedBox(
-                          child: Column(
-                            children: [
-                              InkWell(
-                                child: Icon(
-                                  Icons.remove_circle_outline,
+                  child: BlocBuilder<ProductListCubit, List<ProductEntity>>(
+                    bloc: _productListCubit,
+                    builder: (context, state) {
+                      if (state.isEmpty) {
+                        return Center(
+                          child: Text('Nenhum item adicioando.'),
+                        );
+                      }
+                      return ListView.builder(
+                          itemCount: state.length,
+                          itemBuilder: (context, index) {
+                            ProductEntity productEntity = state[index];
+                            return Container(
+                              margin: const EdgeInsets.only(bottom: 15),
+                              child: Card(
+                                child: ListTile(
+                                  contentPadding: const EdgeInsets.symmetric(
+                                    vertical: 15,
+                                    horizontal: 10,
+                                  ),
+                                  title: Text(
+                                    productEntity.name,
+                                    maxLines: 2,
+                                    style: TextStyle(
+                                        overflow: TextOverflow.ellipsis),
+                                  ),
+                                  subtitle: Container(
+                                    margin: const EdgeInsets.only(top: 10),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                            'Preço: R\$ ${productEntity.price.toStringAsFixed(2)}'),
+                                        const SizedBox(
+                                          height: 5,
+                                        ),
+                                        Text(
+                                            'Total: R\$ ${productEntity.totalPrice.toStringAsFixed(2)}'),
+                                      ],
+                                    ),
+                                  ),
+                                  trailing: FittedBox(
+                                    child: Column(
+                                      children: [
+                                        InkWell(
+                                          child: const Icon(
+                                            Icons.remove_circle_outline,
+                                          ),
+                                          onTap: () {
+                                            _productListCubit
+                                                .decrementProduct(index);
+                                          },
+                                        ),
+                                        Container(
+                                            margin: const EdgeInsets.symmetric(
+                                                vertical: 10),
+                                            child: Text(
+                                                '${productEntity.quantity}')),
+                                        InkWell(
+                                          child: const Icon(
+                                            Icons.add_circle,
+                                            color: Colors.black,
+                                          ),
+                                          onTap: () {
+                                            _productListCubit
+                                                .incrementProduct(index);
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ),
                                 ),
-                                onTap: () {},
                               ),
-                              Container(
-                                  margin: EdgeInsets.symmetric(vertical: 10),
-                                  child: Text('2')),
-                              InkWell(
-                                child: Icon(
-                                  Icons.add_circle,
-                                  color: Colors.black,
-                                ),
-                                onTap: () {},
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
+                            );
+                          });
+                    },
                   ),
                 ),
+                BlocBuilder<ProductListCubit, List<ProductEntity>>(
+                    bloc: _productListCubit,
+                    builder: (context, state) {
+                      return Container(
+                        height: 40,
+                        margin: const EdgeInsets.only(top: 10, bottom: 10),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                        ),
+                        decoration: BoxDecoration(
+                          color: Colors.black12,
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text('TOTAL'),
+                            Text(
+                                'R\$ ${_productListCubit.totalValue.toStringAsFixed(2)}'),
+                          ],
+                        ),
+                      );
+                    }),
                 Container(
-                  height: 40,
-                  margin: EdgeInsets.only(top: 10, bottom: 10),
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 10,
-                  ),
-                  decoration: BoxDecoration(
-                      color: Colors.black12,
-                      borderRadius: BorderRadius.circular(50)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('TOTAL'),
-                      Text('R\$ 200,00'),
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 15),
+                  margin: const EdgeInsets.only(bottom: 15),
                   width: constraints.maxWidth * 0.8,
                   child: ElevatedButton(
                     onPressed: () {},
-                    child: Text('FINALIZAR'),
+                    child: const Text('FINALIZAR'),
                   ),
                 ),
               ],
