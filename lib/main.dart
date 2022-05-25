4a94421ac6ac5a7f47ec86f738ad8886f2b05f14
Antonio Mateus de Souza Medeiros <mateus.medeiros@altislab.com.br>
2022-05-25 20:32:44 -0300

    Alterado página inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:lista_compras_mercado/app/presentation/product_list/pages/product_list_page.dart';
+import 'package:lista_compras_mercado/app/presentation/purchase_list.dart/pages/purchase_list_page.dart';
+import 'package:lista_compras_mercado/app/utils/inject_dependencies.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  await InjectDependencies.load();
   runApp(const MyApp());
 }
 
@@ -16,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const ProductListPage(),
+      home: const PurchaseListPage(),
     );
   }
 }
