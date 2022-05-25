17c603292f2824f6356164dcbdb1569e862e81f5
Antonio Mateus de Souza Medeiros <mateus.medeiros@altislab.com.br>
2022-05-25 20:32:16 -0300

    Adicionado injeção de dependencia

---
+++ lib/app/utils/inject_dependencies.dart
@@ -0,0 +1,27 @@
+import 'package:get_it/get_it.dart';
+import 'package:lista_compras_mercado/app/domain/usecases/imp/get_all_purchases_usecase_imp.dart';
+import 'package:lista_compras_mercado/app/domain/usecases/imp/save_purchase_usecase_imp.dart';
+import 'package:lista_compras_mercado/app/infra/datasources/imp/purchase_datasource_imp.dart';
+import 'package:lista_compras_mercado/app/infra/repositories/purchase_repositoty_imp.dart';
+import 'package:lista_compras_mercado/app/presentation/product_list/bloc/finalize_purchase_bloc.dart';
+import 'package:lista_compras_mercado/app/presentation/purchase_list.dart/bloc/purchase_list_bloc.dart';
+
+class InjectDependencies {
+  static load() {
+    GetIt getIt = GetIt.instance;
+
+    if (!getIt.isRegistered<FinalizePurchaseBloc>()) {
+      getIt.registerFactory<FinalizePurchaseBloc>(
+        () => FinalizePurchaseBloc(SavePurchaseUseCaseImp(
+            PurchaseRepositoryImp(PurchaseDatasourceImp()))),
+      );
+    }
+
+    if (!getIt.isRegistered<PurchaseListBloc>()) {
+      getIt.registerFactory<PurchaseListBloc>(
+        () => PurchaseListBloc(GetAllPurchasesUseCaseImp(
+            PurchaseRepositoryImp(PurchaseDatasourceImp()))),
+      );
+    }
+  }
+}
