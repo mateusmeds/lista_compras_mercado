ffbcfb862ae6600fe83f968ff4fddf2788e85802
Antonio Mateus de Souza Medeiros <mateus.medeiros@altislab.com.br>
2022-05-26 00:41:05 -0300

    Adicionado funções repetidas em um arquivo

---
+++ lib/app/utils/functions.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+showSuccessMessage(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(message),
+    backgroundColor: Colors.green,
+  ));
+}
+
+showErrorMessage(BuildContext context, String message) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(message),
+    backgroundColor: Colors.red,
+  ));
+}
